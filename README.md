a959508155e0c8737ff398ebd1a02015fe41eebb
shermp <14854761+shermp@users.noreply.github.com>
2018-09-12 18:33:55 +1200

    Added licensing information

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Go Callibre Connection
+# UNCaGED - Universal Networked Calibre Go Ereader Device
 A library to connect to Calibre over a network, using Calibre's "Smart Devices" API
+
+## Usage
+UNCaGED is not designed to be run standalone. Rather, it should be integrated with a device specific program to provide a complete "wireless device" solution to connect with Calibre.
+
+You can obtain UNCaGED with
+```go get github.com/shermp/UNCaGED```
+
+Import UNCaGED as
+```
+import (
+    "github.com/shermp/UNCaGED/uncgd"
+)
+```
+
+See `example/main.go` for example usage of the library.
+
+## License
+UNCaGED is licensed under the GPL3 licensing terms.
+
+Please refer to the LICENSE file in the UNCaGED directory for further information
--- example/main.go
+++ example/main.go
@@ -1,3 +1,23 @@
+/*
+	UNCaGED - Universal Networked Calibre Go Ereader Device
+    Copyright (C) 2018 Sherman Perry
+
+    This file is part of UNCaGED.
+
+    UNCaGED is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UNCaGED is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UNCaGED.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
