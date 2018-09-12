073a6b0b8e38adfa8edee3bfe94abf62c469fb02
shermp <14854761+shermp@users.noreply.github.com>
2018-09-12 18:04:11 +1200

    Renamed project to UNCaGED

--- example/main.go
+++ example/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/shermp/GoCalConn/calconn"
+	"github.com/shermp/UNCaGED/uncgd"
 )
 
 func main() {
@@ -14,19 +14,19 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(curDir)
-	var opts calconn.ClientOptions
-	opts.ClientName = "Go Client"
+	var opts uncgd.ClientOptions
+	opts.ClientName = "UNCaGED"
 	opts.CoverDims.Height = 530
 	opts.CoverDims.Width = 530
-	opts.DeviceModel = "Golang"
-	opts.DeviceName = "Go Test"
+	opts.DeviceModel = "Win"
+	opts.DeviceName = "UNCaGED Alpha"
 	opts.DevStore.RootDir = curDir
 	opts.DevStore.BookDir = "exampleBooks"
 	opts.DevStore.LocationCode = "main"
 	opts.DevStore.UUID = "498e8f45-b57f-4fb0-9cba-8c7dae1efb39"
 	opts.SupportedExt = []string{"epub", "mobi"}
 
-	c, err := calconn.New(opts)
+	c, err := uncgd.New(opts)
 	cc := &c
 	if err != nil {
 		fmt.Print(err)
@@ -36,9 +36,9 @@ func main() {
 		for {
 			status := <-cc.Status
 			switch status.StatCode {
-			case calconn.PrintMsg:
+			case uncgd.PrintMsg:
 				fmt.Println(status.Value)
-			case calconn.TCPclosed:
+			case uncgd.TCPclosed:
 				break S
 			}
 		}
--- uncgd/calibrestructs.go
+++ uncgd/calibrestructs.go
@@ -1,4 +1,4 @@
-package calconn
+package uncgd
 
 import "time"
 
